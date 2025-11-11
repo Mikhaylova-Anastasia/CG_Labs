bdeb3331a448d97c638f37694aa8808b34b3c5ab
Mikhaylova-Anastasia <anatirockstar@gmail.com>
2025-11-11 18:48:14 +0300

    Добавила перспективное искажение

--- geometry.h
+++ geometry.h
@@ -5,40 +5,21 @@
 #include <iostream>
 
 template <class t> struct Vec2 {
-    union {
-        struct { t u, v; };
-        struct { t x, y; };
-        t raw[2];
-    };
+    union { struct { t u, v; }; struct { t x, y; }; t raw[2]; };
     Vec2() : u(0), v(0) {}
     Vec2(t _u, t _v) : u(_u), v(_v) {}
-
-
-    template <class U>
-    Vec2(const Vec2<U>& other) : u(static_cast<t>(other.u)), v(static_cast<t>(other.v)) {}
-
+    template <class U> Vec2(const Vec2<U>& o) : u((t)o.u), v((t)o.v) {}
     inline Vec2<t> operator +(const Vec2<t>& V) const { return Vec2<t>(u + V.u, v + V.v); }
     inline Vec2<t> operator -(const Vec2<t>& V) const { return Vec2<t>(u - V.u, v - V.v); }
     inline Vec2<t> operator *(float f)          const { return Vec2<t>(u * f, v * f); }
-
     template <class> friend std::ostream& operator<<(std::ostream& s, const Vec2<t>& v);
 };
 
 template <class t> struct Vec3 {
-    union {
-        struct { t x, y, z; };
-        struct { t ivert, iuv, inorm; };
-        t raw[3];
-    };
+    union { struct { t x, y, z; }; struct { t ivert, iuv, inorm; }; t raw[3]; };
     Vec3() : x(0), y(0), z(0) {}
     Vec3(t _x, t _y, t _z) : x(_x), y(_y), z(_z) {}
-
-
-    template <class U>
-    Vec3(const Vec3<U>& other)
-        : x(static_cast<t>(other.x)), y(static_cast<t>(other.y)), z(static_cast<t>(other.z)) {
-    }
-
+    template <class U> Vec3(const Vec3<U>& o) : x((t)o.x), y((t)o.y), z((t)o.z) {}
     inline Vec3<t> operator ^(const Vec3<t>& v) const { return Vec3<t>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
     inline Vec3<t> operator +(const Vec3<t>& v) const { return Vec3<t>(x + v.x, y + v.y, z + v.z); }
     inline Vec3<t> operator -(const Vec3<t>& v) const { return Vec3<t>(x - v.x, y - v.y, z - v.z); }
@@ -46,7 +27,6 @@ template <class t> struct Vec3 {
     inline t       operator *(const Vec3<t>& v) const { return x * v.x + y * v.y + z * v.z; }
     float norm() const { return std::sqrt(x * x + y * y + z * z); }
     Vec3<t>& normalize(t l = 1) { *this = (*this) * (l / norm()); return *this; }
-
     template <class> friend std::ostream& operator<<(std::ostream& s, const Vec3<t>& v);
 };
 
@@ -55,14 +35,69 @@ typedef Vec2<int>   Vec2i;
 typedef Vec3<float> Vec3f;
 typedef Vec3<int>   Vec3i;
 
-template <class t> std::ostream& operator<<(std::ostream& s, const Vec2<t>& v) {
-    s << "(" << v.x << ", " << v.y << ")\n";
-    return s;
+template <class t> std::ostream& operator<<(std::ostream& s, const Vec2<t>& v) { s << "(" << v.x << ", " << v.y << ")\n"; return s; }
+template <class t> std::ostream& operator<<(std::ostream& s, const Vec3<t>& v) { s << "(" << v.x << ", " << v.y << ", " << v.z << ")\n"; return s; }
+
+
+struct Vec4f {
+    float x, y, z, w;
+    Vec4f() :x(0), y(0), z(0), w(1) {}
+    Vec4f(float X, float Y, float Z, float W = 1) :x(X), y(Y), z(Z), w(W) {}
+};
+
+struct Mat4 {
+    float m[4][4];
+    Mat4() { for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) m[i][j] = (i == j ? 1.f : 0.f); }
+    static Mat4 identity() { return Mat4(); }
+
+    static Mat4 viewport(float x, float y, float w, float h, float depth) {
+        Mat4 V;
+        V.m[0][0] = w / 2.f; V.m[0][3] = x + w / 2.f;
+        V.m[1][1] = h / 2.f; V.m[1][3] = y + h / 2.f;
+        V.m[2][2] = depth / 2.f; V.m[2][3] = depth / 2.f;
+        return V;
+    }
+
+
+    static Mat4 perspective(float fov_deg, float aspect, float znear, float zfar) {
+        Mat4 P;
+
+        for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) P.m[i][j] = 0.f;
+        float f = 1.f / std::tan((fov_deg * 3.1415926535f / 180.f) * 0.5f);
+        P.m[0][0] = f / aspect;
+        P.m[1][1] = f;
+        P.m[2][2] = (zfar + znear) / (znear - zfar);
+        P.m[2][3] = (2.f * zfar * znear) / (znear - zfar);
+        P.m[3][2] = -1.f;
+
+        return P;
+    }
+
+    static Mat4 lookat(const Vec3f& eye, const Vec3f& center, const Vec3f& up) {
+        Vec3f z = (eye - center); z.normalize();
+        Vec3f x = (up ^ z); x.normalize();
+        Vec3f y = (z ^ x);
+        Mat4 M;
+        M.m[0][0] = x.x; M.m[0][1] = x.y; M.m[0][2] = x.z; M.m[0][3] = -(x * eye);
+        M.m[1][0] = y.x; M.m[1][1] = y.y; M.m[1][2] = y.z; M.m[1][3] = -(y * eye);
+        M.m[2][0] = z.x; M.m[2][1] = z.y; M.m[2][2] = z.z; M.m[2][3] = -(z * eye);
+        return M;
+    }
+};
+
+inline Mat4 operator*(const Mat4& A, const Mat4& B) {
+    Mat4 R;
+    for (int i = 0; i < 4; i++) for (int j = 0; j < 4; j++) {
+        R.m[i][j] = 0;
+        for (int k = 0; k < 4; k++) R.m[i][j] += A.m[i][k] * B.m[k][j];
+    }
+    return R;
 }
 
-template <class t> std::ostream& operator<<(std::ostream& s, const Vec3<t>& v) {
-    s << "(" << v.x << ", " << v.y << ", " << v.z << ")\n";
-    return s;
+inline Vec4f operator*(const Mat4& A, const Vec4f& v) {
+    float r[4]; float x[4] = { v.x,v.y,v.z,v.w };
+    for (int i = 0; i < 4; i++) { r[i] = 0; for (int k = 0; k < 4; k++) r[i] += A.m[i][k] * x[k]; }
+    return Vec4f(r[0], r[1], r[2], r[3]);
 }
 
 #endif //__GEOMETRY_H__
--- main.cpp
+++ main.cpp
@@ -12,88 +12,109 @@ const int depth = 255;
 
 Model* model = nullptr;
 float* zbuffer = nullptr;
-Vec3f light_dir(0.f, 0.f, -1.f);
+Vec3f  light_dir(0.f, 0.f, -1.f);
 
 static Vec3f barycentric(const Vec3f* pts, const Vec2f& p) {
     Vec3f a = pts[1] - pts[0];
     Vec3f b = pts[2] - pts[0];
     Vec3f s = Vec3f(a.x, b.x, pts[0].x - p.u) ^ Vec3f(a.y, b.y, pts[0].y - p.v);
-    if (std::fabs(s.z) < 1e-2f) return Vec3f(-1.f, 1.f, 1.f);
+    if (std::fabs(s.z) < 1e-2f) return Vec3f(-1, 1, 1);
     return Vec3f(1.f - (s.x + s.y) / s.z, s.x / s.z, s.y / s.z);
 }
 
-static void triangle_textured(const Vec3i(&screen)[3],
+static void triangle_textured_persp(const Vec4f(&clip)[3],
     const Vec2f(&uvs)[3],
     float intensity,
     TGAImage& image,
     float* zbuffer,
-    Model* model) {
-    Vec3f pts[3] = {
-        Vec3f((float)screen[0].x, (float)screen[0].y, (float)screen[0].z),
-        Vec3f((float)screen[1].x, (float)screen[1].y, (float)screen[1].z),
-        Vec3f((float)screen[2].x, (float)screen[2].y, (float)screen[2].z)
-    };
+    const Mat4& Viewport,
+    Model* model)
+{
 
-    int minx = std::max(0, (int)std::floor(std::min({ screen[0].x, screen[1].x, screen[2].x })));
-    int miny = std::max(0, (int)std::floor(std::min({ screen[0].y, screen[1].y, screen[2].y })));
-    int maxx = std::min(width - 1, (int)std::ceil(std::max({ screen[0].x, screen[1].x, screen[2].x })));
-    int maxy = std::min(height - 1, (int)std::ceil(std::max({ screen[0].y, screen[1].y, screen[2].y })));
+    if (clip[0].w <= 0 && clip[1].w <= 0 && clip[2].w <= 0) return;
 
-    for (int y = miny; y <= maxy; y++) {
-        for (int x = minx; x <= maxx; x++) {
+    Vec3f scr[3];
+    float invw[3];
+    for (int i = 0; i < 3; i++) {
+        invw[i] = 1.f / clip[i].w;
+        Vec4f ndc(clip[i].x * invw[i], clip[i].y * invw[i], clip[i].z * invw[i], 1.f);
+        Vec4f sv = Viewport * ndc;            // z òåïåðü â [0..depth], ãäå ÌÅÍÜØÅ = ÁËÈÆÅ
+        scr[i] = Vec3f(sv.x, sv.y, sv.z);
+    }
+
+    int minx = std::max(0, (int)std::floor(std::min({ scr[0].x, scr[1].x, scr[2].x })));
+    int maxx = std::min(width - 1, (int)std::ceil(std::max({ scr[0].x, scr[1].x, scr[2].x })));
+    int miny = std::max(0, (int)std::floor(std::min({ scr[0].y, scr[1].y, scr[2].y })));
+    int maxy = std::min(height - 1, (int)std::ceil(std::max({ scr[0].y, scr[1].y, scr[2].y })));
+
+    Vec3f pts[3] = { scr[0], scr[1], scr[2] };
+    Vec2f uv_over_w[3];
+    for (int i = 0; i < 3; i++) uv_over_w[i] = uvs[i] * invw[i];
+
+    float shade = std::max(0.f, std::min(1.f, intensity));
+
+    for (int y = miny; y <= maxy; ++y) {
+        for (int x = minx; x <= maxx; ++x) {
             Vec3f bc = barycentric(pts, Vec2f((float)x, (float)y));
-            if (bc.x < 0.f || bc.y < 0.f || bc.z < 0.f) continue;
+            if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue;
+
 
             float z = pts[0].z * bc.x + pts[1].z * bc.y + pts[2].z * bc.z;
+
             int idx = x + y * width;
-            if (zbuffer[idx] >= z) continue;
+            if (z >= zbuffer[idx]) continue;
             zbuffer[idx] = z;
 
-            Vec2f uv = uvs[0] * bc.x + uvs[1] * bc.y + uvs[2] * bc.z;
-            TGAColor tex = model->diffuse(uv);
 
-            float shade = std::max(0.f, std::min(1.f, intensity));
-            unsigned char r = (unsigned char)((float)tex.r * shade);
-            unsigned char g = (unsigned char)((float)tex.g * shade);
-            unsigned char b = (unsigned char)((float)tex.b * shade);
-            image.set(x, y, TGAColor(r, g, b, 255));
+            float invw_p = invw[0] * bc.x + invw[1] * bc.y + invw[2] * bc.z;
+            Vec2f uvw = uv_over_w[0] * bc.x + uv_over_w[1] * bc.y + uv_over_w[2] * bc.z;
+            Vec2f uv = uvw * (1.f / invw_p);
+
+            TGAColor tex = model->diffuse(uv);
+            image.set(x, y, TGAColor((unsigned char)(tex.r * shade),
+                (unsigned char)(tex.g * shade),
+                (unsigned char)(tex.b * shade), 255));
         }
     }
 }
 
 int main(int argc, char** argv) {
-    if (argc == 2) {
-        model = new Model(argv[1]);
-    }
-    else {
-        model = new Model("obj/head.obj");
-    }
+    model = (argc == 2) ? new Model(argv[1]) : new Model("obj/head.obj");
     model->load_texture("obj/head_diffuse.tga");
+    light_dir.normalize();
+
+
+    float fov_deg = 60.f;
+    float aspect = (float)width / (float)height;
+    float znear = 0.2f;
+    float zfar = 10.f;
+
+    Vec3f eye(0, 0, 2.0f), center(0, 0, 0), up(0, 1, 0);
+
+    Mat4 ModelView = Mat4::lookat(eye, center, up);
+    Mat4 Projection = Mat4::perspective(fov_deg, aspect, znear, zfar);
+    Mat4 Viewport = Mat4::viewport(0, 0, (float)width, (float)height, (float)depth);
+    Mat4 MVP = Projection * ModelView;
+
 
     zbuffer = new float[width * height];
-    for (int i = 0; i < width * height; i++) {
-        zbuffer[i] = -std::numeric_limits<float>::max();
-    }
+    for (int i = 0; i < width * height; i++) zbuffer[i] = std::numeric_limits<float>::infinity();
 
     TGAImage image(width, height, TGAImage::RGB);
 
     for (int i = 0; i < model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
+        std::vector<int> f = model->face(i);
         std::vector<int> fuv = model->uvface(i);
 
-        Vec3i screen[3];
+        Vec4f clip[3];
         Vec3f world[3];
         Vec2f uvs[3];
 
         for (int j = 0; j < 3; j++) {
-            Vec3f v = model->vert(face[j]);
-            screen[j] = Vec3i(
-                (int)((v.x + 1.f) * width * 0.5f),
-                (int)((v.y + 1.f) * height * 0.5f),
-                (int)((v.z + 1.f) * depth * 0.5f)
-            );
+            Vec3f v = model->vert(f[j]);
             world[j] = v;
             uvs[j] = model->uv(fuv[j]);
+            clip[j] = MVP * Vec4f(v.x, v.y, v.z, 1.f);
         }
 
         Vec3f n = (world[2] - world[0]) ^ (world[1] - world[0]);
@@ -101,24 +122,11 @@ int main(int argc, char** argv) {
         float intensity = n * light_dir;
         if (intensity <= 0.f) continue;
 
-        triangle_textured(screen, uvs, intensity, image, zbuffer, model);
+        triangle_textured_persp(clip, uvs, intensity, image, zbuffer, Viewport, model);
     }
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
-
-    TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            float z = zbuffer[x + y * width];
-            unsigned char v = (z <= -std::numeric_limits<float>::max() / 2.f)
-                ? 0
-                : (unsigned char)std::max(0.f, std::min(255.f, z));
-            zbimage.set(x, y, TGAColor(v, 1));
-        }
-    }
-    zbimage.flip_vertically();
-    zbimage.write_tga_file("zbuffer.tga");
 
     delete model;
     delete[] zbuffer;
